c76e042a385dad8bdea6174d5545b8c96baad064
Solovewicz <solovewicz>
2016-05-19 09:45:10 -0700

    Adding Chapter 3

---
+++ node-hero_chapter3/app/index.js
@@ -0,0 +1,11 @@
+// app/index.js
+
+const fs = require('fs')  
+
+let content  
+try {  
+  content = fs.readFileSync('file.md', 'utf-8')
+} catch (ex) {
+  console.log(ex)
+}
+console.log(content)  
