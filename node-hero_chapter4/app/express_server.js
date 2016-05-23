b0d77f5b6df36d3f5348801bfb1b5d24cc769d17
Solovewicz <solovewicz>
2016-05-23 13:16:59 -0700

    Add semi-colons to express_server.js

--- node-hero_chapter4/app/express_server.js
+++ node-hero_chapter4/app/express_server.js
@@ -1,17 +1,17 @@
 // app/express_server.js
 
-const express = require('express')  
-const app = express()  
-const port = 3000
+const express = require('express'); 
+const app = express();  
+const port = 3000;
 
 app.get('/', (request, response) => {  
-    response.send('Hello from Express!')
-})
+    response.send('Hello from Express!');
+});
 
 app.listen(port, (err) => {  
     if (err) {
-          return console.log('something bad happened', err)
+          return console.log('something bad happened', err);
 	    }
 
-      console.log(`server is listening on ${port}`)
-})
+      console.log(`server is listening on ${port}`);
+});
