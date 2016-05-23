eaa47f798c775d48102a22264270f44b2dc883bd
Solovewicz <solovewicz>
2016-05-23 13:39:43 -0700

    Added an error handling example to show how it works.

--- node-hero_chapter4/app/express_server.js
+++ node-hero_chapter4/app/express_server.js
@@ -14,9 +14,14 @@ app.use((request, response, next) => {
 });
 
 app.get('/', (request, response) => {  
-  response.json({
-    chance: request.chance
-  });
+  throw new Error('oops');  
 });
+
+app.use((err, request, response, next) => {  
+  // log the error, for now just console.log
+  console.log(err)
+  response.status(500).send('Something broke!');
+})
+
 
 app.listen(3000);  
