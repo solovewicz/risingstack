20f9da2d0fbf5645341a8dc98fff6e4106e39fa3
Solovewicz <solovewicz>
2016-05-23 13:26:32 -0700

    Adding uncommitted files

--- node-hero_chapter3/app/index.js
+++ node-hero_chapter3/app/index.js
@@ -1,11 +1,39 @@
 // app/index.js
 
-const fs = require('fs')  
+const fs = require('fs');  
 
-var content  
+var content;  
 try {  
-  content = fs.readFileSync('file.md', 'utf-8')
+  //Blocking Operation
+  //so this will run asynchronously
+  console.log("Blocking Operation");
+  content = fs.readFileSync('file.md', 'utf-8');
 } catch (ex) {
-  console.log(ex)
+  console.log(ex);
 }
-console.log(content)  
+console.log("Blocking: " + content);  
+
+//non blocking
+
+console.log("Non-Blocking Operation");
+fs.readFile('file.md', 'utf-8', function (err, content) {  
+    if (err) {
+          return console.log(err);
+	    }
+
+      console.log("Non-Blocking: " + content);
+})
+
+
+console.log('start reading a file...');
+
+fs.readFile('file.md', 'utf-8', function (err, content) {  
+  if (err) {
+    console.log('error happened during reading the file');
+    return console.log(err);
+  }
+
+  console.log(content);
+})
+
+console.log('end of the file');  
