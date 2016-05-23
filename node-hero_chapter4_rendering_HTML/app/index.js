22a30ea355c53dc70b9e58af0be92758fa6ed996
Solovewicz <solovewicz>
2016-05-23 15:06:32 -0700

    Adding chapter 4 project which shows a working Handlebars templated Express app.

---
+++ node-hero_chapter4_rendering_HTML/app/index.js
@@ -0,0 +1,50 @@
+// app/index.js
+//to debug express start server with this command
+// DEBUG=express* node index.js
+
+const express = require('express'); 
+const app = express();
+const port = 3000;
+
+const path = require('path');  
+const exphbs = require('express-handlebars');
+
+app.engine('.hbs', exphbs({  
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname, 'views/layouts')
+}));
+
+app.set('view engine', '.hbs'); 
+app.set('views', path.join(__dirname, 'views')); 
+
+app.use((request, response, next) => {  
+  console.log(request.headers);
+  next();
+});
+
+app.use((request, response, next) => {  
+  request.chance = Math.random();
+  next();
+});
+
+//throw an error immidiately to show how error handling works
+app.get('/', (request, response) => {  
+  response.render('home', {
+    name: 'John'
+  });  
+});
+
+/*
+    The error handler function should be the last function a:dded with app.use.
+    The error handler has a next callback - it can be used to chain multiple error handlers.
+*/
+/*
+app.use((err, request, response, next) => {  
+  // log the error, for now just console.log
+  console.log(err);
+  response.status(500).send('Something broke!');
+});
+*/
+
+app.listen(port);  
---
+++ node-hero_chapter4_rendering_HTML/index.js
@@ -0,0 +1,4 @@
+// index.js
+
+require('./app/index')  
+
